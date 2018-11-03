423b055feb8a9b99206a6e663bb4286789f44d01
Xizhen Yang <yangyxz16@gmail.com>
2018-11-02 21:40:02 -0500

    first commit

---
+++ script1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir dir1
+touch dir1/file10
+
+mkdir dir2
+touch dir2/file20
+
+touch file1
+
+ln -s dir2/file20 link1
+
+
---
+++ script2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+find -newer script2.sh >> a.txt
+ls

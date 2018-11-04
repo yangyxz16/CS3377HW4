cf930317028d9d987810b569dc39b208d660847c
Xizhen Yang <yangyxz16@gmail.com>
2018-11-04 17:53:45 -0600

    Test for ls commend using system()

--- 4.3/myprog1.c
+++ 4.3/myprog1.c
@@ -1,51 +1,12 @@
-#include<stdio.h>
-#include<fcntl.h>
-#include<errno.h>
 
-main(int argc, char**argv) {
-	int fd;
-	
-	fd = mkdir("dir1", O_CREAT | S_IRUSR | S_IWUSR | S_IXUSR);
-	printf("%d\n", fd);
-	if(fd == -1) {
-		fprintf(stderr, "Error Number %/d\n", errno);
-		perror("Program");
-		}
-	
-	fd = open("dir1/file10", O_RDONLY | O_CREAT);
-	printf("%d\n", fd);
-	if(fd == -1) {
-		fprintf(stderr, "Error Number %/d\n", errno);
-		perror("Program");
-		}
-	
-	fd = mkdir("dir2", O_CREAT | S_IRUSR | S_IWUSR | S_IXUSR);
-	printf("%d\n", fd);
-	if(fd == -1) {
-		fprintf(stderr, "Error Number %/d\n", errno);
-		perror("Program");
-	}
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-	fd = open("dir2/file20", O_RDONLY | O_CREAT);
-	printf("%d\n", fd);
-     	if(fd == -1) {
-		fprintf(stderr, "Error Number %/d\n", errno);
-		perror("Program");
-	}
-
-	fd = open("file1", O_RDONLY | O_CREAT);
-	printf("%d\n", fd);
-	 if(fd == -1) {
-    	       fprintf(stderr, "Error Number %/d\n", errno);	
-	       perror("Program");
-	 }
-
-	fd = symlink("dir2/file20", "link1");
-	printf("%d\n", fd);
-	 if(fd == -1) {
-         	 fprintf(stderr, "Error Number %/d\n", errno);	
-		 perror("Program");
-		 }
-
-}
-
+int main () {
+	system("ls");
+	//char command[50];
+	//strcpy(command, "ls -l" );
+        //system(command);  
+	return(0);
+	} 
